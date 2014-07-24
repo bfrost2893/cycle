68d580cf47aa1df67be1a7b8cc925f56f75697ce
Brad Frost <bfrost@umich.edu>
2014-07-23 22:33:51 -0400

    empty structure

--- cycle.sql
+++ cycle.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost:8889
--- Generation Time: Jul 24, 2014 at 02:20 AM
+-- Generation Time: Jul 24, 2014 at 04:33 AM
 -- Server version: 5.5.34
 -- PHP Version: 5.5.10
 
@@ -28,4 +28,4 @@ CREATE TABLE `trips` (
   `duration` decimal(10,0) NOT NULL,
   `tripdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
